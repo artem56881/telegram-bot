f804e4fe92a890bb1c0de14b6e1cbd0ff9d4966a
Andrew Sultanov <vysotnik3@gmail.com>
2024-10-06 17:55:58 +0500

    Создал сущность User, добавил метод создания таблицы в класс DatabaseConnection.

--- src/main/java/org/example/config/DatabaseConnection.java
+++ src/main/java/org/example/config/DatabaseConnection.java
@@ -3,6 +3,7 @@ package org.example.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/TelegramBot";
@@ -12,11 +13,31 @@ public class DatabaseConnection {
     public static Connection connect() {
         Connection connection = null;
         try {
+            Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Подключение к базе данных выполнено успешно!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Драйвер PostgreSQL не найден: " + e.getMessage());
         } catch (SQLException e) {
             System.out.println("Ошибка подключения к базе данных: " + e.getMessage());
         }
         return connection;
+    }
+
+    public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Users ("
+                + "id SERIAL PRIMARY KEY, "
+                + "username VARCHAR(50) NOT NULL, "
+                + "email VARCHAR(50) NOT NULL UNIQUE"
+                + ");";
+
+        try (Connection conn = connect(); // Используем метод connect для получения соединения
+             Statement stmt = conn.createStatement()) {
+
+            stmt.executeUpdate(sql);
+            System.out.println("Таблица Users успешно создана.");
+        } catch (SQLException e) {
+            System.out.println("Ошибка создания таблицы: " + e.getMessage());
+        }
     }
 }
