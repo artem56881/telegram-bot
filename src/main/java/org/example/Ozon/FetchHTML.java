d006d3e781d4efd2dad045707185c45c99de270f
Andrew Sultanov <vysotnik3@gmail.com>
2024-11-22 12:23:47 +0500

    Добавил парсер, который Артём переписал на Java, с использованием JSOAP

---
+++ src/main/java/org/example/Ozon/FetchHTML.java
@@ -0,0 +1,35 @@
+package org.example.Ozon;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import static org.example.ProductInfoCollector.collectProductInfo;
+
+
+public class FetchHTML {
+    public static void main(String[] args) {
+        String url = "https://www.ozon.ru/product/nabor-bokalov-dlya-vina-ozon-home-provans-360-ml-6-sht-1171098530/?campaignId=509&oos_search=false";
+
+        // Create an HttpClient instance
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            // Create a GET request
+            HttpGet httpGet = new HttpGet(url);
+//            httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
+
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                String htmlContent = EntityUtils.toString(response.getEntity());
+//                System.out.println(htmlContent);
+                String ozonCardPrice = collectProductInfo(htmlContent).get("product_ozon_card_price");
+                System.out.print("Цена с озон картой: ");
+
+                System.out.println(ozonCardPrice);
+            }
+        } catch (Exception e) {
+            System.err.println("Error fetching the HTML content: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
